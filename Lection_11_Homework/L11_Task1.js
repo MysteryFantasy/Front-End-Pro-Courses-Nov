00b467e36f483c567a680f9ef694f3ef8d63c60c
MysteryFantasy <mystery.fantasy132@gmail.com>
2019-12-27 02:38:32 +0200

    Создала класс SuperMath и его новый экземпляр p

---
+++ Lection_11_Homework/L11_Task1.js
@@ -0,0 +1,17 @@
+function SuperMath() {
+
+}
+
+console.log(SuperMath, 'SuperMath');
+
+
+obj = {
+    X: 12,
+    Y: 3,
+    znak: "/"
+}
+
+console.log(obj, 'obj');
+
+var p = new SuperMath();
+console.log(p, 'p');
