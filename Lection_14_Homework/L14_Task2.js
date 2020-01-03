361f7c9d6dfc9d0f9471cd728276a79aa3994aad
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-04 01:37:22 +0200

    Добавила плавное перемещение div.box в место клика, используя document.onclick

--- Lection_14_Homework/L14_Task2.js
+++ Lection_14_Homework/L14_Task2.js
@@ -1,10 +1,23 @@
 window.onload = function() {
     var boxik = document.querySelector('.box');
-    console.log(boxik, 'boxik');
+    // console.log(boxik, 'boxik');
 
     boxik.onclick = function() {
         this.classList.toggle('foo');
     }
 
+    document.onclick = function(event) {
+        // console.log(this,'this');
+        // console.log(event.type, 'event.type');
+        console.log(event, 'event');
+        var a = event.clientX;
+        console.log(a, 'clientX');
+        var b = event.clientY;
+        console.log(b,'clientY');
+        
+        boxik.classList.add('position');
+        boxik.style.top = b +'px';
+        boxik.style.left = a + 'px';
+    }
 
 }
