69df726cbe901f760f9827e4e257b9f74db6a30b
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-07 22:51:40 +0200

    Изменила алгоритм перемещения div.box за мышью

--- Lection_14_Homework/L14_Task2.js
+++ Lection_14_Homework/L14_Task2.js
@@ -2,39 +2,42 @@ window.onload = function() {
     var boxik = document.querySelector('.box');
     // console.log(boxik, 'boxik');
 
-    boxik.onclick = function() {
-        this.classList.toggle('foo');
-    }
+    // boxik.onclick = function() {
+    //     this.classList.toggle('foo');
+    // }
 
-    document.onclick = function(event) {
-        // console.log(this,'this');
-        // console.log(event.type, 'event.type');
-        console.log(event, 'event');
-        var a = event.clientX;
-        console.log(a, 'clientX');
-        var b = event.clientY;
-        console.log(b,'clientY');
-        
-        boxik.classList.add('position');
-        boxik.style.top = b +'px';
-        boxik.style.left = a + 'px';
-    }
+    // document.onclick = function() {
+    //     // console.log(event.type, 'event.type');
+    //     console.log(event, 'event');
 
-    boxik.onmousedown = function() {
-        // console.log(this, 'boxik down');
+    //     var a = event.clientX;
+    //     console.log(a, 'clientX');
+    //     var b = event.clientY;
+    //     console.log(b,'clientY');
+
+    //     boxik.classList.add('position');
+    //     boxik.style.left = a + 'px';
+    //     boxik.style.top = b +'px';
+    // }
+
+    boxik.onmousedown = function(event) {
+        var offX = event.offsetX;
+        console.log(offX, 'offX');
+        var offY = event.offsetY;
+        console.log(offY, 'offY');    
 
         document.onmousemove = function(event){
-            var c = event.pageX;
-            // console.log(c, 'pageX');
-            var d = event.pageY;
-            // console.log(d,'pageY');
-            
-            if(event.which == 1) {
-                boxik.style.left = c + 'px';
-                boxik.style.top = d + 'px';
-            }
-    
+            c = event.pageX;
+            console.log(c, 'pageX');
+            d = event.pageY;
+            console.log(d,'pageY');
+            boxik.style.left = c - offX + 'px';
+            boxik.style.top =  d - offY + 'px';
         }
     }
 
+    boxik.onmouseup = function() {
+        document.onmousemove = null;
+    }
+    
 }
