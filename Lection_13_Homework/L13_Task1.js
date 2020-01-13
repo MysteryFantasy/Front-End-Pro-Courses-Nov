40ccec0168752aecc40b1d9535165ab582e03f9c
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-13 14:55:59 +0200

    Написала функцию для получения и вывода в консоль содержимого всех полей input

--- Lection_13_Homework/L13_Task1.js
+++ Lection_13_Homework/L13_Task1.js
@@ -2,8 +2,20 @@ window.onload = function() {
 
     setInterval(function(){
         
-        var usrText = document.querySelectorAll('inputText');
+        var usrText = document.querySelectorAll('.inputText');
         console.log(usrText, 'usrText');
+
+        function resText(mass) {
+            var str = '';
+        
+            for(var i = 0; i < mass.length - 1; i++) {
+                str += mass[i].value + ',';
+            }
+            str += mass[mass.length - 1].value;
+            return str;
+        }
+
+        console.log(resText(usrText));
 
     }, 2000)
 
