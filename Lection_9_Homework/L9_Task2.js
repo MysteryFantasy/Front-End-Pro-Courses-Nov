d7895297437a52fde28980252d7d048b8c399643
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-25 13:58:06 +0200

    Добавила функцию capLetFirst(str) для того, чтобы сделать первые буквы строки заглавными. Применила эту функцию к arr[i], начиная со второго элемента в массиве

--- Lection_9_Homework/L9_Task2.js
+++ Lection_9_Homework/L9_Task2.js
@@ -5,11 +5,19 @@ function changeText(str) {
   var arr = str.split('_');
   console.log(arr, 'arr');
 
-  for(var i = 0; i < arr.length; i++){
+  for(var i = 1; i < arr.length; i++){
+    arr[i] = capLetFirst(arr[i]);
     console.log(arr[i], 'arr[i]')
   }
-
   return arr;
+}
+
+function capLetFirst(str) {
+  if (!str) {
+    return str;
+  } else {
+    return str[0].toUpperCase() + str.slice(1);
+  }
 }
 
 console.log(changeText(str, 'str'));
