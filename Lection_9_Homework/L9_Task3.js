424dbf7bc61d2892ef4e4f4ad4d8c0ada2d64bef
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-25 14:41:47 +0200

    Добавила в функцию переменную elem, которой присваивается элемент массива, содержащий заданную строку. В inArray(str,arr) сделала вывод true/false при проверке наличия в arr заданной str

--- Lection_9_Homework/L9_Task3.js
+++ Lection_9_Homework/L9_Task3.js
@@ -1,16 +1,17 @@
 function inArray(str, arr) {
     // console.log(str, 'str');
     // console.log(arr, 'arr');
+    var elem;
 
     for(var i = 0; i < arr.length; i++) {
-        // console.log(arr[i], 'arr[i]');
         if(arr[i].includes(str)){
-            console.log(arr[i] + " This element has required string");
+            elem = arr[i];
+            console.log(elem + " This element has required string");
         } else {
             console.log(arr[i] + " This element doesn't have required string");
         }
     }
-
+    return elem ? true : false;
 }
 
-inArray('foo', ['sjhfnaof', 'affooasf', 'dfhdfhdfh']);
+console.log(inArray('foo', ['sjhfnaof', 'affooasf', 'dfhdfhdfh']));
