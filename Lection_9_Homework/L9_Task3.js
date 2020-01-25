e6612d89001097cc14529c40ff6346e72568c563
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-25 14:18:06 +0200

    При помощи цикла пробежалась по всем элементам массива

--- Lection_9_Homework/L9_Task3.js
+++ Lection_9_Homework/L9_Task3.js
@@ -1,6 +1,11 @@
 function inArray(str, arr) {
-    console.log(str, 'str');
-    console.log(arr, 'arr');
+    // console.log(str, 'str');
+    // console.log(arr, 'arr');
+    
+    for(var i = 0; i < arr.length; i++) {
+        console.log(arr[i], 'arr[i]');
+    }
+
 }
 
 inArray('foo', ['sjhfnaof', 'affooasf', 'dfhdfhdfh']);
