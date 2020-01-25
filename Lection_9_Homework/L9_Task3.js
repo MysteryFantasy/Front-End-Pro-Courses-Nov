bf16a2799c13a8e168cd8999c0217da7c3fda44f
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-01-25 14:24:11 +0200

    При помощи метода includes() проверила, содержит ли элемент массива заданную str

--- Lection_9_Homework/L9_Task3.js
+++ Lection_9_Homework/L9_Task3.js
@@ -1,9 +1,14 @@
 function inArray(str, arr) {
     // console.log(str, 'str');
     // console.log(arr, 'arr');
-    
+
     for(var i = 0; i < arr.length; i++) {
-        console.log(arr[i], 'arr[i]');
+        // console.log(arr[i], 'arr[i]');
+        if(arr[i].includes(str)){
+            console.log(arr[i] + " This element has required string");
+        } else {
+            console.log(arr[i] + " This element doesn't have required string");
+        }
     }
 
 }
