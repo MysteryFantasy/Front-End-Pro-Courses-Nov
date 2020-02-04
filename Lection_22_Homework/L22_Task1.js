b5a010ccb351789d0aa5ccd19f0a8fbc2bfc44a7
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-02-05 01:13:43 +0200

    Используя цепочку Promise, при клике на confirm() организовала вывод в консоль такой порядок цифр: да -> 0, 1 для resolve() и нет -> 0, 2 для reject()

---
+++ Lection_22_Homework/L22_Task1.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    let promise = new Promise(function(resolve, reject){
+        console.log(0);
+        setTimeout(function(){
+            if(confirm()){
+                resolve();
+            } else{
+                reject();
+            }
+        }, 2000);
+    });
+
+    promise.then(function(){
+        console.log(1, 'resolve 1');
+    },
+
+    function() {
+        console.log(2, 'reject 1');
+        return;
+    });
+
+}
