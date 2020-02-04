f48910cc23f5ca6cecdccb944fdbebf7c786821b
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-02-04 23:25:43 +0200

    Добавила еще 2 блока с кнопкой Click и счетчиком counter. Их функционал соответствует 1 блоку. Изменила запись значениий счетчиков в localStorage. Для этого создала объект storage{} с  методами setInStorage(){} и getFromStorage(){}

--- Lection_18_Homework/L18_Task1.js
+++ Lection_18_Homework/L18_Task1.js
@@ -1,21 +1,36 @@
 window.onload = function() {
-    var button = document.querySelector('.btn');
+    var button = document.querySelectorAll('.btn');
     // console.log(button, 'button');
-    
-    var counter = document.querySelector('.count');
-    // console.log(counter,'counter');
-    
-    var size = '';
-    size = localStorage.getItem('counter');
 
-    counter.innerHTML = size;
+    var storage = {
+        value: '',
+        setInStorage: function(key, value) {
+            localStorage.setItem(key, value);
+        },
+        getFromStorage: function(key) {
+            this.value = localStorage.getItem(key) || 0;
+        }
+    }
 
-    button.onclick = function(){
-        console.log('click')
-        
-        size = ++counter.innerHTML;
-        localStorage.setItem('counter', size);
+    function getParent(elem, selector) {
+        return elem.closest(selector);
+    }
 
+    for(let i = 0; i < button.length; i ++) {
+        storage.getFromStorage('counter' + i);
+      
+        var block = getParent(button[i], '.block').querySelector('.count').innerHTML = storage.value;       
+
+        button[i].onclick = function() {
+            block = getParent(button[i], '.block');
+
+            var counter = 0;
+
+            if (block) {
+                counter = ++block.querySelector('.count').innerHTML;
+            }
+            storage.setInStorage('counter' + i, counter);
+        }
     }
 
 }
