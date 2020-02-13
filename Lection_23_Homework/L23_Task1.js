d252f752c2f79c72605971a6a9d6f9406486a904
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-02-14 01:56:57 +0200

    Добавила проверку if/else, которая при отсутствии элементов в списке, оповещает об этом в консоле

--- Lection_23_Homework/L23_Task1.js
+++ Lection_23_Homework/L23_Task1.js
@@ -6,19 +6,27 @@ var TaskManager = {
         toDoList1: [{task:'Get up'}, {task:'Cook breakfast'}, {task: 'Read a book'}, {task: 'Eat lunch'}, {task:'Go to bed'}],
         toDoList2: [{task: 'Order pizza'}, {task: 'Go for a walk'}, {task:'Listen to music'}, {task: 'Do homework'}, {task: 'Watch TV'}],
         transferToList2(){
-            var last1 = this.toDoList1.pop();
-            let item1 = last1.task;
-            console.log(item1, 'item1');
-            this.toDoList2.unshift({task: item1});
+            if(this.toDoList1.length !== 0){
+                var last1 = this.toDoList1.pop();
+                let item1 = last1.task;
+                console.log(item1, 'item1');
+                this.toDoList2.unshift({task: item1});
+            } else {
+                console.log('No tasks to do');
+            }
         },
         transferToList1(){
-            var last2 = this.toDoList2.pop();
-            let item2 = last2.task;
-            console.log(item2, 'item2');
-            this.toDoList1.unshift({task: item2});
+            if(this.toDoList2.length !== 0){
+                var last2 = this.toDoList2.pop();
+                let item2 = last2.task;
+                console.log(item2, 'item2');
+                this.toDoList1.unshift({task: item2});
+            } else {
+                console.log('No tasks to do');
+            }
         }
     }
-}
+};
 
 let myVue = new Vue(TaskManager);
 
