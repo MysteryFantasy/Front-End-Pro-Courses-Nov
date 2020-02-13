95e3159b0f57c04818ec15b37dcfaf7638ca1083
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-02-14 01:04:45 +0200

    Добавила кнопку Transfer в конец второго списка. При нажатии на кнопку, последний элемент из списка To Do List 2 переносится в начало первого списка

--- Lection_23_Homework/L23_Task1.js
+++ Lection_23_Homework/L23_Task1.js
@@ -6,10 +6,16 @@ var TaskManager = {
         toDoList1: [{task:'Get up'}, {task:'Cook breakfast'}, {task: 'Read a book'}, {task: 'Eat lunch'}, {task:'Go to bed'}],
         toDoList2: [{task: 'Order pizza'}, {task: 'Go for a walk'}, {task:'Listen to music'}, {task: 'Do homework'}, {task: 'Watch TV'}],
         transferToList2(){
-            var last = this.toDoList1.pop();
-            let item1 = last.task;
+            var last1 = this.toDoList1.pop();
+            let item1 = last1.task;
             console.log(item1, 'item1');
             this.toDoList2.unshift({task: item1});
+        },
+        transferToList1(){
+            var last2 = this.toDoList2.pop();
+            let item2 = last2.task;
+            console.log(item2, 'item2');
+            this.toDoList1.unshift({task: item2});
         }
     }
 }
