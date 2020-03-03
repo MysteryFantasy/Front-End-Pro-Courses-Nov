3707d9ab541351ddba7d1d541a8b564260a77ef6
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-03-03 23:39:32 +0200

    При помощи setInterval и функции setValueInStore(arr), получила данные из полей input

--- Lection_17_Homework/L17_Task1.js
+++ Lection_17_Homework/L17_Task1.js
@@ -1,5 +1,24 @@
 window.onload = function() {
-    var userData = document.querySelectorAll('.firstName, .lastName, .position');
-    console.log(userData, 'userData');  
+    var userData2 = document.querySelectorAll('.firstName, .lastName, .position');
+    console.log(userData2, 'userData');
+    
+    setInterval(function(){
+
+        function setValueInStore(arr) {
+            var mass = new Array;
+            var userInfo = {};
+            
+            for(var i = 0; i < arr.length; i++) {
+                var name = arr[i].name;
+                var value = arr[i].value;
+                userInfo[name] = value;
+                console.log(userInfo, 'userInfo');
+            }
+            mass.unshift(userInfo);
+            console.log(mass, 'mass');
+        };
+        setValueInStore(userData2)
+
+    }, 5000);
 
 }
