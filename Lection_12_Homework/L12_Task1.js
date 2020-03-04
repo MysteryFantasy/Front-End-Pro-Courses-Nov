ee566e32175beb1633deb24f916a7afd774ff4ee
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-03-04 23:39:08 +0200

    Создала метод setMarker({x: 2, y: 3}), который устанавливает маркер '&' в переданную точку массива

--- Lection_12_Homework/L12_Task1.js
+++ Lection_12_Homework/L12_Task1.js
@@ -52,3 +52,20 @@ SuperArray.prototype.clear = function(direction, k) {
 
 arr.clear('row', 1);
 arr.clear('column', 3);
+
+SuperArray.prototype.setMarker = function(position) {
+    var x = position.x;
+    // console.log(x, 'x');
+    var y = position.y;
+    // console.log(y, 'y');
+    marker = "&";
+    // console.log(marker, 'marker');
+
+    for(var i = 0; i < this.mass.length; i++){
+        for(var j = 0; j < this.mass[i].length; j++){
+            this.mass[x][y] = marker;
+        }
+    } 
+};
+
+arr.setMarker({x:2, y:3});
