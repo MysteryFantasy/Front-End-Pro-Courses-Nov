61930a3fbb0c9e307d57f5a49766989a30ec87b3
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-03-08 17:26:03 +0200

    Добавила переход по страничкам при клике на форму

--- Lection_16_Homework/L16_Task1.js
+++ Lection_16_Homework/L16_Task1.js
@@ -1,18 +1,34 @@
 window.onload = function() {
 
     var box = document.querySelectorAll('.boxik');
-    console.dir(box,'box');
-    console.log(box, 'box');
+    // console.dir(box,'box');
+    // console.log(box, 'box');
 
     var button = document.querySelectorAll('.btn');
-    console.dir(button,'button');
-    console.log(button, 'button');
+    // console.dir(button,'button');
+    // console.log(button, 'button');
     
     var firstName = document.querySelectorAll('.firstName');
-    console.log(firstName, 'firstName');
+    // console.log(firstName, 'firstName');
     var lastName = document.querySelectorAll('.lastName');
-    console.log(lastName, 'lastName');
+    // console.log(lastName, 'lastName');
     var age = document.querySelectorAll('.age');
-    console.log(age, 'age');
+    // console.log(age, 'age');
+    
+    var i = 0;
 
+    for(; i < box.length; i++){
+        box[i].onclick = (function(i){
+            return function(){
+                var counter;
+                if(box[i + 1]){
+                    counter = i + 1;    
+                } else {
+                    counter = 0;
+                }
+                this.classList.remove('center');
+                box[counter].classList.add('center');
+            }
+        })(i);
+    };
 }
