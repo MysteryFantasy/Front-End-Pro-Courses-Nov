c0f15ef2d4f4ee21e321e828b534728e34b80830
MysteryFantasy <mystery.fantasy132@gmail.com>
2020-03-08 16:10:26 +0200

    Создала 5 форм с полями input и кнопками

---
+++ Lection_16_Homework/L16_Task1.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+
+    var box = document.querySelectorAll('.boxik');
+    console.dir(box,'box');
+    console.log(box, 'box');
+
+    var button = document.querySelectorAll('.btn');
+    console.dir(button,'button');
+    console.log(button, 'button');
+    
+    var firstName = document.querySelectorAll('.firstName');
+    console.log(firstName, 'firstName');
+    var lastName = document.querySelectorAll('.lastName');
+    console.log(lastName, 'lastName');
+    var age = document.querySelectorAll('.age');
+    console.log(age, 'age');
+
+}
